7b487a2a21c663e12642beefe297bda46b2b5476
Jason Tan <jtan20040401@gmail.com>
2023-08-31 12:44:33 +1000

    Disable security

--- product/src/main/java/com/amazonaws/awssamples/microservices/product/service/ProductService.java
+++ product/src/main/java/com/amazonaws/awssamples/microservices/product/service/ProductService.java
@@ -4,10 +4,10 @@ import com.amazonaws.awssamples.microservices.common.Product;
 import com.amazonaws.awssamples.microservices.common.Review;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.core.AbstractOAuth2Token;
-import org.springframework.security.oauth2.jwt.Jwt;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.security.oauth2.core.AbstractOAuth2Token;
+//import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -60,27 +60,29 @@ public class ProductService {
     @RequestMapping(value="/v2/{id}",method = RequestMethod.GET)
     public Product getProductWithReview(@PathVariable Long id){
         Product product = productMap.get(id);
-        restTemplate.getInterceptors().add((request, body, execution) -> {
-                    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-                    if (authentication == null) {
-                        return execution.execute(request, body);
-                    }
-
-                    if (!(authentication.getCredentials() instanceof AbstractOAuth2Token)) {
-                        return execution.execute(request, body);
-                    }
-
-                    AbstractOAuth2Token token = (AbstractOAuth2Token) authentication.getCredentials();
-                    if (token instanceof Jwt) {
-                        Jwt jwtToken = (Jwt)token;
-                        Map<String, Object> claims = jwtToken.getClaims();
-                        System.out.println("User name: " + claims.get("username"));
-                    }
-
-                    request.getHeaders().setBearerAuth(token.getTokenValue());
-                    return execution.execute(request, body);
-                });
-        Review review = restTemplate.getForObject(reviewUrl + "/service/review/v1/" + id, Review.class);
+//        restTemplate.getInterceptors().add((request, body, execution) -> {
+//                    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//                    if (authentication == null) {
+//                        return execution.execute(request, body);
+//                    }
+//
+//                    if (!(authentication.getCredentials() instanceof AbstractOAuth2Token)) {
+//                        return execution.execute(request, body);
+//                    }
+//
+//                    AbstractOAuth2Token token = (AbstractOAuth2Token) authentication.getCredentials();
+//                    if (token instanceof Jwt) {
+//                        Jwt jwtToken = (Jwt)token;
+//                        Map<String, Object> claims = jwtToken.getClaims();
+//                        System.out.println("User name: " + claims.get("username"));
+//                    }
+//
+//                    request.getHeaders().setBearerAuth(token.getTokenValue());
+//                    return execution.execute(request, body);
+//                });
+        String url = reviewUrl + "/service/review/v1/" + id;
+        System.out.println("URL: " + url);
+        Review review = restTemplate.getForObject(url, Review.class);
         product.setReview(review.getReview());
         productMap.put(id, product);
         System.out.println("here now");
